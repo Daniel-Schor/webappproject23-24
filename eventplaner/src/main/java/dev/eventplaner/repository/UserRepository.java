6a0fe1450133d4ffdcf9e5993a5b40f27c620a7e
Daniel Schor <daniel.schor@gmx.de>
2023-12-23 21:55:33 +0100

    Update key types in repository classes to UUID (before: Long)

--- eventplaner/src/main/java/dev/eventplaner/repository/UserRepository.java
+++ eventplaner/src/main/java/dev/eventplaner/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package dev.eventplaner.repository;
 
 import java.util.HashMap;
+import java.util.UUID;
 
 import org.springframework.stereotype.Repository;
 
 import dev.eventplaner.model.User;
 
 @Repository
-public class UserRepository extends HashMap<Long, User> {
+public class UserRepository extends HashMap<UUID, User> {
 
 
 }
