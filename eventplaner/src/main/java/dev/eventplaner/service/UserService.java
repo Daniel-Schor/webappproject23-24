59a827b9dfeea5076d7412afc3297fd4a036911a
ynnskrnr <126953115+ynnskrnr@users.noreply.github.com>
2023-12-27 01:56:36 +0000

    UserService Kommentare erstellt

--- eventplaner/src/main/java/dev/eventplaner/service/UserService.java
+++ eventplaner/src/main/java/dev/eventplaner/service/UserService.java
@@ -1,25 +1,33 @@
 package dev.eventplaner.service;
 
 import dev.eventplaner.model.User;
-
+import dev.eventplaner.repository.UserRepository;
 import java.util.UUID;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import dev.eventplaner.repository.UserRepository;
-
 @Service
 public class UserService {
 
+    // Logger instance for this class, used to log system messages, warnings, and
+    // errors.
     private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
+    // Autowired annotation is used to automatically inject the UserRepository
+    // instance into this class.
     @Autowired
     private UserRepository userRepository;
 
-    public User getUser(UUID userID)  {
+    /**
+     * Retrieves a user by their ID.
+     *
+     * @param userID The ID of the user to retrieve.
+     * @return The User object corresponding to the given ID, or null if no such
+     *         user exists.
+     */
+    public User getUser(UUID userID) {
         log.info("get user by userID: {}", userID);
         User user = userRepository.get(userID);
         if (user == null) {
@@ -27,12 +35,22 @@ public class UserService {
         }
         return user;
     }
-    
+
+    /**
+     * Creates a new user.
+     *
+     * @param user The User object to create.
+     */
     public void createUser(User user) {
         log.info("User Created: {}", user.getUserID());
         userRepository.put(user.getUserID(), user);
     }
 
+    /**
+     * Deletes a user by their ID.
+     *
+     * @param userID The ID of the user to delete.
+     */
     public void deleteUser(UUID userID) {
         if (userRepository.get(userID) == null) {
             log.warn("User with ID {} not found", userID);
@@ -42,19 +60,27 @@ public class UserService {
             userRepository.remove(userID);
             log.info("User Deleted: {}", userID);
         }
-        
+
     }
 
+    /**
+     * Updates a user.
+     *
+     * @param user The User object to update.
+     */
     public void updateUser(User user) {
         log.info("User Updated: {}", user.getUserID());
         userRepository.put(user.getUserID(), user);
     }
 
+    /**
+     * Retrieves all users.
+     *
+     * @return An Iterable of all User objects.
+     */
     public Iterable<User> getAll() {
         log.info("getAllUsers");
         return userRepository.values();
     }
-
-
 
 }
