c87e9e611b8ca80b0dca59df7ab5e58f06488c5a
Daniel Schor <daniel.schor@gmx.de>
2024-01-01 18:37:08 +0100

    Add home page and index.html

--- eventplaner/src/main/java/dev/eventplaner/controller/WebController.java
+++ eventplaner/src/main/java/dev/eventplaner/controller/WebController.java
@@ -1,10 +1,18 @@
 package dev.eventplaner.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 
-    
+    private static final Logger log = LoggerFactory.getLogger(ApiController.class);
+
+    @GetMapping(value = "/home")
+    public String home() {
+        return "index.html";
+    }
 
 }
