c1fafa23225ae94239730ba6c136ffdee6c09d68
ynnskrnr <126953115+ynnskrnr@users.noreply.github.com>
2024-01-08 17:48:45 +0000

    repository spring project created

---
+++ repository/eventplaner/src/main/java/dev/eventplaner/repository/eventplaner/EventplanerApplication.java
@@ -0,0 +1,13 @@
+package dev.eventplaner.repository.eventplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventplanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventplanerApplication.class, args);
+	}
+
+}
---
+++ repository/eventplaner/src/test/java/dev/eventplaner/repository/eventplaner/EventplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eventplaner.repository.eventplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
