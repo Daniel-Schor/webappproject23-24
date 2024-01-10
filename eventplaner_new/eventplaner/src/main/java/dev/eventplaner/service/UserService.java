124271812a7e5eb8d45e4359b333aa80c98db360
Esrom <136700131+unigithubacc@users.noreply.github.com>
2024-01-10 17:16:40 +0000

    ordnerstruktur angepasst

---
+++ eventplaner_new/eventplaner/src/main/java/dev/eventplaner/service/UserService.java
@@ -0,0 +1,155 @@
+package dev.eventplaner.service;
+
+import dev.eventplaner.model.ApiError;
+import dev.eventplaner.model.User;
+
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class UserService {
+
+    // Logger instance for this class, used to log system messages, warnings, and
+    // errors.
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
+    // Read the URL of the external API from properties file.
+    @Value("${userservice.url}")
+    String apiUrl;
+
+    /**
+     * Retrieves a user by their ID.
+     *
+     * @param userID The ID of the user to retrieve.
+     * @return The User object corresponding to the given ID, or null if no such
+     *         user exists.
+     */
+    public ResponseEntity<?> getUser(UUID userID) {
+        log.info("get user by userID: {}", userID);
+        RestTemplate restTemplate = new RestTemplate();
+        String url = apiUrl + "/users/" + userID;
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+
+        ResponseEntity<?> response;
+
+        try {
+            response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+        } catch (HttpClientErrorException e) {
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, e.getResponseBodyAsString());
+            response = new ResponseEntity<>(apiError, apiError.getStatus());
+        }
+        return response;
+    }
+
+    /**
+     * Creates a new user.
+     *
+     * @param user The User object to create.
+     */
+    public ResponseEntity<?> create(User user) {
+        log.info("User Created: {}", user.getID());
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = apiUrl + "/users";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<User> request = new HttpEntity<User>(user, headers);
+
+        ResponseEntity<?> response;
+
+        try {
+            response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+        } catch (HttpClientErrorException e) {
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, e.getResponseBodyAsString());
+            response = new ResponseEntity<>(apiError, apiError.getStatus());
+        }
+        return response;
+    }
+
+    /**
+     * Deletes a user by their ID.
+     *
+     * @param userID The ID of the user to delete.
+     */
+    public ResponseEntity<?> delete(UUID userID) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = apiUrl + "/users/" + userID;
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+
+        ResponseEntity<?> response;
+
+        try {
+            response = restTemplate.exchange(url, HttpMethod.DELETE, request, String.class);
+        } catch (HttpClientErrorException e) {
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, e.getResponseBodyAsString());
+            response = new ResponseEntity<>(apiError, apiError.getStatus());
+        }
+        return response;
+
+    }
+
+    /**
+     * Updates a user.
+     *
+     * @param user The User object to update.
+     */
+    public ResponseEntity<?> update(User user) {
+        log.info("User Updated: {}", user.getID());
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = apiUrl + "/users";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<User> request = new HttpEntity<User>(user, headers);
+
+        ResponseEntity<?> response;
+
+        try {
+            response = restTemplate.exchange(url, HttpMethod.PUT, request, String.class);
+        } catch (HttpClientErrorException e) {
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, e.getResponseBodyAsString());
+            response = new ResponseEntity<>(apiError, apiError.getStatus());
+        }
+
+        return response;
+    }
+
+    public ResponseEntity<?> getAllDTO() {
+        log.info("get all Events as DTO");
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = apiUrl + "/users";
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+
+        ResponseEntity<?> response;
+
+        try {
+            response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+        } catch (HttpClientErrorException e) {
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, e.getResponseBodyAsString());
+            response = new ResponseEntity<>(apiError, apiError.getStatus());
+        }
+        return response;
+
+    }
+
+}
