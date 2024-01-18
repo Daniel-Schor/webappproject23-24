5d9d7a3be29b34eed8f9e135f8826aca9f048b2b
Davipede99 <davidepedergnana99@gmail.com>
2024-01-18 12:28:05 +0100

    Zeile gelöscht

--- userservice/eventplaner/src/main/java/dev/eventplaner/controller/ApiController.java
+++ userservice/eventplaner/src/main/java/dev/eventplaner/controller/ApiController.java
@@ -16,13 +16,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.eventplaner.model.Event;
 import dev.eventplaner.model.User;
-import dev.eventplaner.model.UserDTO;
+
 import dev.eventplaner.service.UserService;
 import org.springframework.web.bind.annotation.PutMapping;
 
