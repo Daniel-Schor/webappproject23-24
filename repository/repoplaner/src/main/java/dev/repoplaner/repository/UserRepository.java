ec581658c79e6ff2c0e34717668a2561f9517d0f
Tristan Buls <tristan.buls1@gmail.com>
2024-01-21 18:15:05 +0100

    UserRepository Namensänderung

---
+++ repository/repoplaner/src/main/java/dev/repoplaner/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package dev.repoplaner.repository;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import dev.repoplaner.model.User;
+
+@Repository
+public class UserRepository extends HashMap<UUID, User> {
+
+
+}
