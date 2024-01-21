e71138434065b95f7e4051efaaac795ded55efd4
Tristan Buls <tristan.buls1@gmail.com>
2024-01-21 18:16:42 +0100

    EventplanerApplicationTest Namensänderung

---
+++ repository/repoplaner/src/test/java/dev/eventplaner/EventplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.repoplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class repoplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
