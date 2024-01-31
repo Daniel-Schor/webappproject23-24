477a1014410b0a1406ce9fb51d86e6c9d4dcf4f1
Davipede99 <davidepedergnana99@gmail.com>
2024-01-31 01:41:22 +0100

    DTO angepasst

--- eventplaner/src/main/java/dev/eventplaner/model/UserDTO.java
+++ eventplaner/src/main/java/dev/eventplaner/model/UserDTO.java
@@ -1,28 +1,34 @@
 package dev.eventplaner.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class UserDTO {
 
+
+    @JsonProperty("ID")
     UUID userID;
-    String fullName;
+    String firstName, lastName;
     boolean organizer; // Add this line
 
     public UserDTO(User user) {
         this.userID = user.getID();
-        this.fullName = user.getFirstName() + " " + user.getLastName();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
         this.organizer = user.isOrganizer(); // Add this line
     }
 
     public UserDTO(UUID userID, String firstName, String lastName, boolean organizer) {
         this.userID = userID;
-        this.fullName = firstName + " " + lastName;
-        this.organizer = organizer; // Add this line
-    }
-
-    public UserDTO(UUID userID, String fullName, boolean organizer) {
-        this.userID = userID;
-        this.fullName = fullName;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.organizer = organizer; // Add this line
     }
 
@@ -34,19 +40,39 @@ public class UserDTO {
         this.userID = userID;
     }
 
-    public String getFullName() {
-        return fullName;
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
     public boolean isOrganizer() { // Add this method
         return organizer;
     }
 
     public void setOrganizer(boolean organizer) { // Add this method
         this.organizer = organizer;
+    }
+
+     public static Collection<UserDTO> collectionFromJsonUser(String s) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        Collection<UserDTO> values = new ArrayList<>();
+
+        try {
+            values = mapper.readValue(s, new TypeReference<Collection<UserDTO>>() {});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return values;
     }
 }
--- userservice/userplaner/src/main/java/dev/userplaner/model/UserDTO.java
+++ userservice/userplaner/src/main/java/dev/userplaner/model/UserDTO.java
@@ -4,34 +4,52 @@ import java.util.UUID;
 
 public class UserDTO {
 
-    private final UUID userID;
-    private String fullName;
+    UUID userID;
+    String firstName, lastName;
+    boolean organizer; // Add this line
 
     public UserDTO(User user) {
         this.userID = user.getID();
-        this.fullName = user.getFirstName() + " " + user.getLastName();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.organizer = user.isOrganizer(); // Add this line
     }
 
     public UserDTO(UUID userID, String firstName, String lastName, boolean organizer) {
         this.userID = userID;
-        this.fullName = firstName + " " + lastName;
-    }
-
-    public UserDTO(UUID userID, String fullName, boolean organizer) {
-        this.userID = userID;
-        this.fullName = fullName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.organizer = organizer; // Add this line
     }
 
     public UUID getUserID() {
         return userID;
     }
 
-    public String getFullName() {
-        return fullName;
+    public void setUserID(UUID userID) {
+        this.userID = userID;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public boolean isOrganizer() { // Add this method
+        return organizer;
+    }
+
+    public void setOrganizer(boolean organizer) { // Add this method
+        this.organizer = organizer;
+    }
 }
