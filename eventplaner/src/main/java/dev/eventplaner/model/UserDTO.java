b675836dba2a1968c2dfbb02e0eeb983b13f0aa2
Davipede99 <davidepedergnana99@gmail.com>
2024-01-31 16:16:23 +0100

    users html funktioniert, users werden displayed

--- eventplaner/src/main/java/dev/eventplaner/model/UserDTO.java
+++ eventplaner/src/main/java/dev/eventplaner/model/UserDTO.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.UUID;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,23 +12,29 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 public class UserDTO {
 
 
-    @JsonProperty("ID")
     UUID userID;
     String firstName, lastName;
-    boolean organizer; // Add this line
+    boolean organizer; 
+
+    public UserDTO() {
+        this.userID = UUID.randomUUID();
+        this.firstName = "John";
+        this.lastName = "Doe";
+        this.organizer = false; 
+    }
 
     public UserDTO(User user) {
         this.userID = user.getID();
         this.firstName = user.getFirstName();
         this.lastName = user.getLastName();
-        this.organizer = user.isOrganizer(); // Add this line
+        this.organizer = user.isOrganizer(); 
     }
 
     public UserDTO(UUID userID, String firstName, String lastName, boolean organizer) {
         this.userID = userID;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.organizer = organizer; // Add this line
+        this.organizer = organizer; 
     }
 
     public UUID getUserID() {
@@ -55,15 +60,15 @@ public class UserDTO {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    public boolean isOrganizer() { // Add this method
+    public boolean isOrganizer() { 
         return organizer;
     }
 
-    public void setOrganizer(boolean organizer) { // Add this method
+    public void setOrganizer(boolean organizer) { 
         this.organizer = organizer;
     }
 
-     public static Collection<UserDTO> collectionFromJsonUser(String s) {
+    public static Collection<UserDTO> collectionFromJsonUserDTO(String s) {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         Collection<UserDTO> values = new ArrayList<>();
@@ -74,5 +79,18 @@ public class UserDTO {
             e.printStackTrace();
         }
         return values;
+    }
+
+    public static UserDTO userFromJson(String s) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        UserDTO user = new UserDTO(new User());
+
+        try {
+            user = mapper.readValue(s, new TypeReference<UserDTO>() {});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 }
