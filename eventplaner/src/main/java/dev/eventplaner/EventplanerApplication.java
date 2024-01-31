acc445133eb451a23085234ddf392cafc904bbd5
Mavin Scholl <mavin-moris.scholl@stud.fra-uas.de>
2024-01-31 23:02:21 +0100

    Javadoc for EventplanerApplication

--- eventplaner/src/main/java/dev/eventplaner/EventplanerApplication.java
+++ eventplaner/src/main/java/dev/eventplaner/EventplanerApplication.java
@@ -3,9 +3,19 @@ package dev.eventplaner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is the main entry point for the Eventplaner application.
+ * It is annotated with @SpringBootApplication, indicating it is a Spring Boot
+ * application.
+ */
 @SpringBootApplication
 public class EventplanerApplication {
 
+	/**
+	 * The main method that serves as an entry point for the application.
+	 *
+	 * @param args The command-line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EventplanerApplication.class, args);
 	}
